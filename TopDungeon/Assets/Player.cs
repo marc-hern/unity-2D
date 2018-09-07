b690800690c5809acd1af7db871c4fbcd830f484
Marcus Hernandez <mar.hernandez010@gmail.com>
2018-09-06 22:17:41 -0700

    Character movement, sprite changes on movement

---
+++ TopDungeon/Assets/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private BoxCollider2D boxCollider;
+
+	private Vector3 moveDelta;
+
+	// Use this for initialization
+	private void Start () {
+		boxCollider = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	// void Update () {
+		
+	// }
+
+	private void FixedUpdate(){
+		// Reset MoveDelta
+		float x = Input.GetAxisRaw("Horizontal");
+		float y = Input.GetAxisRaw("Vertical");
+		moveDelta = new Vector3(x, y, 0);
+
+		// Swap the sprite direction when moving right or left
+		if (moveDelta.x > 0){
+			transform.localScale = Vector3.one;
+		} 
+		else if (moveDelta.x < 0){
+			transform.localScale = new Vector3(-1, 1, 1);
+		}
+
+		// Move the character move
+		transform.Translate(moveDelta * Time.deltaTime);
+	}
+}
