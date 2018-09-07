65836e247ece563666172762796d97d970692eee
Marcus Hernandez <mar.hernandez010@gmail.com>
2018-09-06 23:30:58 -0700

    Camera motor script added

---
+++ TopDungeon/Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour {
+
+	// lookAt the obj
+	public Transform lookAt;
+
+	public float boundX = 0.15f;
+	public float boundY = 0.05f;
+
+	private void LateUpdate(){
+		Vector3 delta = Vector3.zero;
+	}
+}
