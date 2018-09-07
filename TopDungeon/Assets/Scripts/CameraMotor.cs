53b485fce968e62df91e0ebfcc77200efac7371b
Marcus Hernandez <mar.hernandez010@gmail.com>
2018-09-06 23:37:59 -0700

    Camera follows player, emphasis on x rather than y

--- TopDungeon/Assets/Scripts/CameraMotor.cs
+++ TopDungeon/Assets/Scripts/CameraMotor.cs
@@ -12,5 +12,29 @@ public class CameraMotor : MonoBehaviour {
 
 	private void LateUpdate(){
 		Vector3 delta = Vector3.zero;
+
+		// To check if we're inside the bounds of the X axis
+		float deltaX = lookAt.position.x - transform.position.x;
+		if (deltaX > boundX || deltaX < -boundX){
+			if (transform.position.x < lookAt.position.x){
+				delta.x = deltaX - boundX;
+			}
+			else{
+				delta.x = deltaX + boundX;
+			}
+		}
+
+		// To check if we're inside the bounds of the Y axis
+		float deltaY = lookAt.position.y - transform.position.y;
+		if (deltaY > boundY || deltaY < -boundY){
+			if (transform.position.y < lookAt.position.y){
+				delta.y = deltaY - boundY;
+			}
+			else{
+				delta.y = deltaY + boundY;
+			}
+		}
+
+		transform.position += new Vector3(delta.x, delta.y, 0);
 	}
 }
