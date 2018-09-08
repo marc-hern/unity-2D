d45ee6a8b3f8bfc6b1707b82898a6f0ae708e0d2
Marcus Hernandez <mar.hernandez010@gmail.com>
2018-09-08 15:13:27 -0700

    Game Manager added, save/load state

---
+++ TopDungeon/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	public List<Sprite> playerSprites;
+	public List<Sprite> weaponSprites;
+	public List<int> weaponPrices;
+	public List<int> xpTable;
+
+	public Player player;
+
+	public int gold;
+	public int experience;
+
+	private void Awake(){
+		if (GameManager.instance != null){
+			Destroy(gameObject);
+			return;
+		}
+		instance = this;
+		SceneManager.sceneLoaded += LoadState;
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public void SaveState(){
+		Debug.Log("SaveState");
+	}
+
+	public void LoadState(Scene s, LoadSceneMode mode){
+		Debug.Log("LoadState");
+	}
+	
+}
