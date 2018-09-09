30664ee6db9e8cae133e0b2a0d97f52624148678
Marcus Hernandez <mar.hernandez010@gmail.com>
2018-09-09 09:27:38 -0700

    Mover script, Damage script, fighting complete

--- TopDungeon/Assets/Scripts/Fighter.cs
+++ TopDungeon/Assets/Scripts/Fighter.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Fighter : MonoBehaviour {
 
 	// Public fields
-	public int hitpoint;
-	public int maxHitpoint;
+	public int hitpoint = 10;
+	public int maxHitpoint = 10;
 	public float pushRecoverySpeed = 0.2f;
 
 	// Immunity
@@ -17,20 +17,13 @@ public class Fighter : MonoBehaviour {
 	protected Vector3 pushDirection;
 
 	// All fighters can ReceieveDamage / Die
-	protected virtual void ReceieveDamage(Damage dmg){
+	protected virtual void ReceiveDamage(Damage dmg){
 		if (Time.time - lastImmune > immuneTime){
 			lastImmune = Time.time;
 			hitpoint -= dmg.damageAmount;
 			pushDirection = (transform.position - dmg.origin).normalized * dmg.pushForce;
 
-			GameManager.instance.ShowText(
-				dmg.damageAmount.ToString(),
-				15,
-				Color.red,
-				transform.position,
-				Vector3.zero,
-				0.5f
-			);
+			GameManager.instance.ShowText(dmg.damageAmount.ToString(), 25, Color.red, transform.position, Vector3.zero, 0.5f);
 
 			if (hitpoint <= 0){
 				hitpoint = 0;
--- TopDungeon/Assets/Scripts/Player.cs
+++ TopDungeon/Assets/Scripts/Player.cs
@@ -2,66 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Fighter {
-
-	private BoxCollider2D boxCollider;
-
-	private Vector3 moveDelta;
-
-	private RaycastHit2D hit;
-
-	// Use this for initialization
-	private void Start () {
-		boxCollider = GetComponent<BoxCollider2D>();
-	}
+public class Player : Mover {
 	
-	// Update is called once per frame
-	// void Update () {
-		
-	// }
-
 	private void FixedUpdate(){
-		// Reset MoveDelta
 		float x = Input.GetAxisRaw("Horizontal");
 		float y = Input.GetAxisRaw("Vertical");
-		moveDelta = new Vector3(x, y, 0);
 
-		// Swap the sprite direction when moving right or left
-		if (moveDelta.x > 0){
-			transform.localScale = Vector3.one;
-		} 
-		else if (moveDelta.x < 0){
-			transform.localScale = new Vector3(-1, 1, 1);
-		}
-
-		// Make sure we can move in this direction, by casting a box there first
-		//  if the box returns null we are free to move
-		hit = Physics2D.BoxCast(
-			transform.position,
-			boxCollider.size,
-			0,
-			new Vector2(0, moveDelta.y),
-			Mathf.Abs(moveDelta.y * Time.deltaTime),
-			LayerMask.GetMask("Actor", "Blocking")
-		);
-
-		if (hit.collider == null){
-			// Move the character move
-			transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
-		}
-
-		hit = Physics2D.BoxCast(
-			transform.position,
-			boxCollider.size,
-			0,
-			new Vector2(moveDelta.x, 0),
-			Mathf.Abs(moveDelta.x * Time.deltaTime),
-			LayerMask.GetMask("Actor", "Blocking")
-		);
-
-		if (hit.collider == null){
-			// Move the character move
-			transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
-		}
+		UpdateMotor(new Vector3(x, y, 0));
 	}
 }
